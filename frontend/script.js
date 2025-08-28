05c9678069554beb0e8ee8f9104b1b5ca9316f06
harshita211106 <114283416+harshita211106@users.noreply.github.com>
2025-08-28 23:46:31 +0530

    updated

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,7 @@
 async function getRecipe() {
   const ingredients = document.getElementById("ingredients").value;
 
-  const response = await fetch("http://localhost:5000/recommend", {
+  const response = await fetch("http://localhost:5000/api/generate", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ ingredients })
